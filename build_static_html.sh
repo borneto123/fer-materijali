beaa4e92311bea57f47136e1d8194ca9e74008c9
LeKosfrancuz <mateo.kos100@gmail.com>
2024-10-31 00:21:47 +0100

    Added a basic static html build system - the gm file is a modified version of https://github.com/kpym/gm, a small wraper   around the markdown parser https://github.com/yuin/goldmark - math typesetting is done with katex

---
+++ build_static_html.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PORT="8080"
+
+# Remove old build
+rm -rf ./build/
+
+# Move all files to build
+cp -r . ./build/
+
+# Remove build from itself
+rm -rf ./build/build
+
+# Code Highlighting themes:
+# github-dark, onedark, arduino, catppuccin-latte, catppuccin-frappe, monokailight, nord, xcode, xcode-dark, vs, pastie
+# Test and find more available themes on https://swapoff.org/chroma/playground/
+HL_THEME="catppuccin-latte"
+
+# Generate HTML from markdown files
+./gm **/*.md --move-no-md -o build --gm-highlighting $HL_THEME
+
+# Remove extra markdown files
+find ./build -name '*.md' -exec rm './{}' ';'
+
+# Remove .git directories and config
+find ./build -name '*.yml' -exec rm './{}' ';'
+rm -rf ./build/.git
+rm -f ./build/.gitignore
+
+
+
+if [ "$1" = "run" ]; then
+	pushd build
+	echo "Serving HTTP on localhost:$PORT"
+	python3 -m http.server $PORT 2>&1 | tee ../server.log --ignore-interrupts
+	popd
+fi
